5eda3fd4a9fdcc7c0293f134998600b4af58e846
miryã <db220355>
2022-03-30 16:58:08 -0300

    incialização

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function FazGet(url){
+
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+
+}
+
+function CriaLinha(usuario){
+
+
+}
+
+function main(){
+
+
+
+}
